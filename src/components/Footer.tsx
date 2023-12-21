5136d3e22068c1fb11bfccc454dcae2d101ac551
StevenYAMBOS <stevenyambos@gmail.com>
2023-12-21 23:57:16 +0100

    Design du Footer
    Composant footer stylisé mais pas encore fonctionnel.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,36 @@
-import React from "react"
-
 const Footer = () => {
-    return (
-        <footer>
-            <h1>Footer</h1>
-        </footer>
-    )
-}
+  return (
+    <>
+      <footer className="w-full h-auto bg-black absolute bottom-0">
+        <div className="footer-wrapper w-9/12 h-full m-auto py-4 px-4 block justify-center text-center md:flex md:flex-row md:justify-between">
 
-export default Footer
+          <div className="logo-wrapper flex justify-center">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              alt="Logo Douceurs Artisanales"
+            />
+          </div>
+
+          <div className="nav-menu block">
+            <div className="block font-medium text-center py-4 px-4 md:flex md:flex-row lg:flex lg:flex-row">
+            <div className="p-2">
+                <a href="/" className="text-white">Accueil</a>
+            </div>
+            <div className="p-2">
+                <a href="/" className="text-white">Où sont mes desserts ?</a>
+            </div>
+            <div className="p-2">
+                <a href="/" className="text-white">Histoires</a>
+            </div>
+            <div className="p-2">
+                <a href="/" className="text-white">Contact</a>
+            </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
+import { useState } from "react";
 
 const Navbar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
